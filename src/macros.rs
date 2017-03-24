5d0cd20f9b597595bcd49d956f1913da36f072b8
Machtan <jako3047@gmail.com>
2017-03-24 23:39:42 +0100

    Document the process macro a little

--- src/macros.rs
+++ src/macros.rs
@@ -1,5 +1,123 @@
 extern crate froggy;
 
+/// Declares a component with the given name and type.
+#[macro_export]
+macro_rules! component {
+    ( $name:ident : $type:ty) => {
+        /// A component.
+        #[derive(Debug, Clone, Copy)]
+        pub struct $name;
+        
+        impl traits::CompId for $name {
+            type Type = $type;
+        }
+    }
+}
+
+/// Declares a component with the members inside a created module.
+/// The process takes a set of mutable and immutable components as arguments,
+/// as declared with the `mut` and `ref` arguments.
+/// 
+/// The body of the run function is executed in a context, in which the
+/// components have been loaded and converted to their associated types.
+/// The run function can also have extra `ext` arguments declared, that
+/// are just passed directly to the scope.
+#[macro_export]
+macro_rules! process {
+    (
+        $( #[$meta:meta] )*
+        pub mod $mod:ident {
+            $proc_id:ident::run( 
+                // Mutable components, always first.
+                $( mut $mut_arg:ident[$mut_gensym:ident] : &mut $mut_comp:ident, )*
+                
+                // Immutable components.
+                $( ref $arg:ident[$gensym:ident] : & $comp:ident, )*
+                
+                // External arguments (relevant here?)
+                $( ext $ext_arg:ident : $ext_ty:ty, )*
+            ) $body:block
+        }
+    ) => {
+        $( #[$meta] )*
+        pub mod $mod {
+            use super::traits;
+            use super::froggy;
+            use std::fmt::Debug;
+            $(
+                use super::$mut_comp;
+            )*
+            $(
+                use super::$comp;
+            )*
+            
+            /// Indices to arguments of this process.
+            pub type ArgRefs = ( $( froggy::StorageRc<<$comp as traits::CompId>::Type> ),* );
+            
+            // Arguments to this function
+            // pub type Args = ( $( &mut $mut_comp, )* $( &$comp, )* );
+            
+            /// Identifies this process.
+            pub struct $proc_id;
+            
+            impl traits::ProcId for $proc_id {
+                type ArgRefs = self::ArgRefs;
+            }
+            
+            unsafe impl<T> traits::HasProc<self::$proc_id> for T 
+              where T: traits::HasProcStore<self::$proc_id>
+                  $( + traits::HasCompStore<$mut_comp> )*
+                  $( + traits::HasCompStore<$comp> )*
+            {}
+            
+            impl $proc_id {
+                pub fn run<S>(sim: &mut S $(, $ext_arg : $ext_ty )* )
+                  where S: traits::HasProc<self::$proc_id> 
+                         + traits::HasProcStore<self::$proc_id>
+                      $( + traits::HasCompStore<$mut_comp> )*
+                      $( + traits::HasCompStore<$comp> )*
+                {
+                    $(  
+                        let mut $mut_arg = <S as traits::HasCompStore<$mut_comp>>::get_mut_components(sim).write();
+                    )*
+                    $(
+                        let $arg = <S as traits::HasCompStore<$comp>>::get_components(sim).read();
+                    )*
+                    
+                    for &( $( ref $mut_gensym, )* $( ref $gensym, )* )
+                    in &<S as traits::HasProcStore<self::$proc_id>>::process_members(sim).read() {
+                        $(
+                            let $mut_arg = $mut_arg.get_mut($mut_gensym);
+                        )*
+                        $(
+                            let $arg = $arg.get($gensym);
+                        )*
+                        $body
+                    }
+                }
+            }
+            
+            // Add the debug clause to allow the concatenation of bounds.
+            // Could as well be a useless blanket implemented trait.
+            impl<T> traits::IntoProcArgs<self::$proc_id> for T
+              where T: Debug 
+                       $( + traits::HasComp<$mut_comp> )*
+                       $( + traits::HasComp<$comp> )*
+            {
+                fn into_args(&self) -> self::ArgRefs {
+                    (
+                        $(<T as traits::HasComp<$mut_comp>>::get(self).clone() , )* 
+                        $(<T as traits::HasComp<$comp>>::get(self).clone() , )*
+                    )
+                }
+            }
+        }
+        
+        // Bring the id into scope 
+        pub use self::$mod::$proc_id;
+    }
+}
+
 /// Declares a new entity, with its members contained in the module with the given name.
 ///
 /// # Generation example
@@ -130,20 +248,6 @@ macro_rules! entity {
             }
         )*
     }
-    }
-}
-
-/// Declares a component with the given name and type.
-#[macro_export]
-macro_rules! component {
-    ( $name:ident : $type:ty) => {
-        /// A component.
-        #[derive(Debug, Clone, Copy)]
-        pub struct $name;
-        
-        impl traits::CompId for $name {
-            type Type = $type;
-        }
     }
 }
 
